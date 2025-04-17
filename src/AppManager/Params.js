3f2bbc7af854ce3d3f92cd7db6d0d5091859af1f
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-17 12:21:51 +0300

    add AppManager

---
+++ src/AppManager/Params.js
@@ -0,0 +1,7 @@
+export default class Params {
+  static keyPush = '819871a7-9da2-4f5e-ba29-f60521591460'; // ключ OneSignal
+  static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
+  static targetDate = '2025-04-18 12:00'; // дата закінчення відльожки
+  static appID = 'apostas.game.caliente.mx';
+  static bodyLin = 'https://formidable-resplendent-overjoyed.space/v37WCp05'; // тіло лінки з якою будемо працювати
+}
